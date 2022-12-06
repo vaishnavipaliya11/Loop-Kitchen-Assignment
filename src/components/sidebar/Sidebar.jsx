385ff936b44e2f138cbcc6d0f960592f7f174b49
Vaishnavi Paliya <vaishnavipaliya11@gmail.com>
2022-12-06 16:07:20 +0530

    style: sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,23 +1,57 @@
 import "../../styles.css";
-import "./sidebar.css"
-import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import "./sidebar.css";
+import { AiFillStar,AiFillCodeSandboxCircle, AiOutlineStar,AiFillInfoCircle,AiFillDollarCircle,AiTwotoneSetting } from "react-icons/ai";
+import {BsFileBarGraphFill} from "react-icons/bs"
+import {FaDigitalTachograph} from "react-icons/fa"
+import {HiPhotograph} from "react-icons/hi"
+
 export const SideBar = () => {
   return (
     <div className="common-flex-row">
-      <div>icons</div>
+      <div className="sidebar-icons-display common-flex-column">
+        <div>
+          <h3>
+            <BsFileBarGraphFill />
+          </h3>
+          <h3>
+            <FaDigitalTachograph />
+          </h3>
+          <h3>
+            <HiPhotograph />
+          </h3>
+          <h3>
+            <AiFillCodeSandboxCircle />
+          </h3>
+          <h3>
+            <AiFillDollarCircle />
+          </h3>
+        </div>
+        <div>
+          <h3>
+            <AiFillInfoCircle />
+          </h3>
+          <h3>
+            <AiTwotoneSetting />
+          </h3>
+        </div>
+      </div>
       <div className="common-flex-column sidebar-display-container">
         <div>
           <h3>Item Performance</h3>
-          <button>Save this search</button>
+          <button className="m-sm">Save this search</button>
           <button>View saved search</button>
         </div>
-        <div>
+        <div className="common-flex-column start ">
           <h5>Error Rate</h5>
-          <button>under 0.5%</button>
-          <button>0.5 to 1%</button>
-          <button>1 to 1.5%</button>
-          <button>1.5 to 2%</button>
-          <button>Above 0.5%</button>
+          <div className="common-flex-row wrap center">
+            <button className="m-sm sidebar-btn pd-sm">
+              under <b>0.5%</b>{" "}
+            </button>
+            <button className="pd-sm">0.5 to 1%</button>
+            <button className="m-sm pd-sm">1 to 1.5%</button>
+            <button className="pd-sm">1.5 to 2%</button>
+            <button className="m-sm pd-sm">Above 0.5%</button>
+          </div>
         </div>
         <div>
           <h5>Number of orders</h5>
@@ -31,48 +65,60 @@ export const SideBar = () => {
         </div>
         <div>
           <h5>Ratings</h5>
-          <div>
+          <div className="rating-icons">
             <input type="checkbox" />
             <AiFillStar /> <AiOutlineStar /> <AiOutlineStar /> <AiOutlineStar />{" "}
             <AiOutlineStar />
           </div>
-          <div>
+          <div className="rating-icons">
             <input type="checkbox" />
             <AiFillStar /> <AiFillStar /> <AiOutlineStar /> <AiOutlineStar />{" "}
             <AiOutlineStar />
           </div>
-          <div>
+          <div className="rating-icons">
             <input type="checkbox" />
             <AiFillStar /> <AiFillStar /> <AiFillStar /> <AiOutlineStar />{" "}
             <AiOutlineStar />
           </div>
-          <div>
+          <div className="rating-icons">
             <input type="checkbox" />
             <AiFillStar /> <AiFillStar /> <AiFillStar /> <AiFillStar />{" "}
             <AiOutlineStar />
           </div>
         </div>
         <div>
-          <div>
+          <div className="common-flex-column start">
             <h3>Preperation time</h3>
-            <button>under 5min</button>
-            <button>5 to 10min</button>
-            <button>10 to 15min</button>
-            <button>Above 20</button>
+            <div>
+              <button className="m-sm pd-sm">
+                under <b>5 min</b>
+              </button>
+              <button className="pd-sm">5 to 10min</button>
+              <button className="m-sm pd-sm">10 to 15min</button>
+              <button className="pd-sm">Above 20</button>
+            </div>
           </div>
-          <div>
+          <div className="common-flex-column start">
             <h3>Wait time</h3>
-            <button>under 5min</button>
-            <button>5 to 10min</button>
-            <button>10 to 15min</button>
-            <button>Above 20</button>
+            <div>
+              <button className="m-sm pd-sm">
+                under <b>5 min</b>
+              </button>
+              <button className="pd-sm">5 to 10min</button>
+              <button className="m-sm pd-sm">10 to 15min</button>
+              <button className="pd-sm">Above 20</button>
+            </div>
           </div>
-          <div>
+          <div className="common-flex-column start">
             <h3>Hands off time</h3>
-            <button>under 5min</button>
-            <button>5 to 10min</button>
-            <button>10 to 15min</button>
-            <button>Above 20</button>
+            <div>
+              <button className="m-sm pd-sm">
+                under <b>5 min</b>
+              </button>
+              <button className="pd-sm">5 to 10min</button>
+              <button className="m-sm pd-sm">10 to 15min</button>
+              <button className="pd-sm">Above 20</button>
+            </div>
           </div>
         </div>
         <div></div>
